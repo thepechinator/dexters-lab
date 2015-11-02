3db2e57a5a8dcc507bc6d880cf75f0c17005eb9d
Paul Pechin <ppechin@usnews.com>
2015-11-01 22:07:57 -0500

    Get animations working with nested slides.

--- src/js/views/super-deck.js
+++ src/js/views/super-deck.js
@@ -7,39 +7,61 @@ export default class SuperDeck {
     this.animatedExit = false;
     this.animating = false;
 
+    // Tells us what slides have what parent.
+    this.slideParentMap = new WeakMap();
+
     this.$slides = $context.find('.slide');
+
+    this.$slides.each( (index, el) => {
+      let $el = $(el);
+      this.slideParentMap.set(el, this.findSlideParent($el));
+    });
+    console.log(this.slideParentMap);
 
     // This can at least help us with the navigation.
     // Hopefully we can just hook into its events.
     this.navEngine(this.$slides);
 
-    this.setupEventListeners();
+    this.setupBasicTransitions();
     this.constructAnimations();
   }
 
-  setupEventListeners() {
+  setupBasicTransitions() {
     $(document).on('deck.change', (event, from, to) => {
       let $toSlide = this.navEngine('getSlide', to);
       let $fromSlide = this.navEngine('getSlide', from);
 
-      TweenMax.set($fromSlide, {clearProps:"right"});
+      // Don't animate if the slides share the same parent.
+      if (this.slideParentMap.get($fromSlide[0]) === this.slideParentMap.get($toSlide[0])) {
+        return;
+      }
+
+      TweenMax.set(this.slideParentMap.get($fromSlide[0]), {clearProps:"right"});
       // from and to are indices
-      console.log($toSlide);
       if (to > from) {
         // moving forward
-        TweenMax.from($toSlide, 0.5, {css: {right: "-=100%"}})
+        TweenMax.from(this.slideParentMap.get($toSlide[0]), 0.5, {css: {right: "-=100%"}})
       } else {
         // moving backward
-        console.log('moving backward');
-        TweenMax.from($toSlide, 0.5, {css: {right: "+=100%"}})
+        console.log('moving backward', this.slideParentMap.get($toSlide[0]));
+        TweenMax.from(this.slideParentMap.get($toSlide[0]), 0.5, {css: {right: "+=100%"}})
       }
     });
 
     // We need to have an exit animation for the slide we're leaving,
     // because I think it looks cooler.
     $(document).on('deck.beforeChange', (event, from, to) => {
+      console.log(event);
+      let $fromSlide = this.navEngine('getSlide', from);
+      let $toSlide = this.navEngine('getSlide', to);
+
       if (from == to) {
         event.preventDefault();
+        return;
+      }
+
+      // Don't animate if the slides share the same parent.
+      if (this.slideParentMap.get($fromSlide[0]) === this.slideParentMap.get($toSlide[0])) {
         return;
       }
 
@@ -49,7 +71,6 @@ export default class SuperDeck {
 
         if (!this.animatedExit) {
           this.animating = true;
-          let $fromSlide = this.navEngine('getSlide', from);
           let moveDir = "+=100%";
 
           // This should be the exact opposite of the incoming slide.
@@ -59,7 +80,8 @@ export default class SuperDeck {
             moveDir = "-=100%";
           }
 
-          TweenMax.to($fromSlide, 0.5,
+          $('body').scrollTop(0);
+          TweenMax.to(this.slideParentMap.get($fromSlide[0]), 0.5,
             {css: {right: moveDir}, onComplete: () => {
               console.log('animation complete');
               this.animatedExit = true;
@@ -73,6 +95,18 @@ export default class SuperDeck {
       }
 
     });
+  }
+
+  findSlideParent($slide) {
+    let $el = $slide;
+    while ($el.length) {
+      if ($el.hasClass('js-slide-parent')) {
+        return $el[0];
+      }
+      $el = $el.parent();
+    }
+
+    return null;
   }
 
   constructAnimations() {
